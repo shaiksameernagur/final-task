1ff4909df44a1634f6601edd55daf98f275396ae
shaiksameernagur <93675106+shaiksameernagur@users.noreply.github.com>
2021-11-03 23:32:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSJO_efz_2cqkC5LOmZ-h2wEh6skXoc38",
+  authDomain: "final-project-1f241.firebaseapp.com",
+  projectId: "final-project-1f241",
+  storageBucket: "final-project-1f241.appspot.com",
+  messagingSenderId: "884445925922",
+  appId: "1:884445925922:web:8c40fa8b3ab47ec13cb07b"
+};
